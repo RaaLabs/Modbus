52d9737661fb2553275eb4cefd21455e0da1e91c
Einar Ingebrigtsen <einar@dolittle.com>
2019-06-04 14:29:05 +0200

    Module scaffolding up and running

---
+++ Source/Program.cs
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Dolittle.TimeSeries.Modules;
+using Dolittle.TimeSeries.Modules.Booting;
+using Newtonsoft.Json;
+
+namespace Dolittle.TimeSeries.Modbus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bootloader.Configure(_ => {}).Start().Wait();
+        }
+    }
+}
