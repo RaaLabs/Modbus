46efc2b9ba08a903d118523d520ebf56394c952f
Einar Ingebrigtsen <einar@dolittle.com>
2019-07-03 19:38:18 +0200

    FunctionCode

---
+++ Source/FunctionCode.cs
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace Dolittle.TimeSeries.Modbus
+{
+    /// <summary>
+    /// Represents a Modbus function code
+    /// </summary>
+    /// <remarks>
+    /// http://www.simplymodbus.ca/FAQ.htm#FC
+    /// </remarks>
+    public enum FunctionCode
+    {
+        /// <summary>
+        /// Discrete output coil
+        /// </summary>
+        Coil=1,
+
+        /// <summary>
+        /// Holding registers
+        /// </summary>
+        HoldingRegister,
+
+        /// <summary>
+        /// Discrete input contacts
+        /// </summary>
+        Input,
+        
+        /// <summary>
+        /// Input register
+        /// </summary>
+        InputRegister
+    }
+}
