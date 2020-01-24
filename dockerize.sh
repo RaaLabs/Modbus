94738f41662d72110f533481d9cdacdeecdaabb7
sigbjorn <sigbjorn.rudaa@gmail.com>
2020-01-24 11:49:27 +0100

    New features: define interval

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
-docker build --no-cache -f ./Source/Dockerfile -t dolittle/timeseries-modbus:$VERSION . --build-arg CONFIGURATION="Release"
-docker push dolittle/timeseries-modbus:$VERSION
+docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-modbus:$VERSION . --build-arg CONFIGURATION="Release"
+docker push shipos/timeseries-modbus:$VERSION
